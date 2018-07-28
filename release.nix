d62e0e66b4be0be1dfba3d78ab577b4c4564f45e
Kirill Elagin <kirelagin@gmail.com>
2018-07-28 19:37:09 +0300

    Update stackage-nix

--- release.nix
+++ release.nix
@@ -4,8 +4,8 @@ let
   stackageRev = "b69b9987e989c925c2bc467f409b259fe248106f";
   stackageSha = "0p31hnnn6fdni3mr142bwnl0kdgx39m3jc9py3qvdfy1nbdmvakz";
 
-  stackageNixRev = "71f025af0171aef133584efbf5c532ebc7aab0d1";
-  stackageNixSha = "12mvpf5wbwn7h2raa92p83f7s5fbp8vblwz4jswkbsya5kxhfrlf";
+  stackageNixRev = "bbb01f8c59a4afaa4ddf3c00f94be5d91ada89f8";
+  stackageNixSha = "0sh17cgbm03ln24whhqwr4la8ksbb3cwlncm61xfh59yr8qlaa5q";
 in
 
 import ./. {
